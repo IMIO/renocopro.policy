48fc99a5504c86a526531f9c71037ddf309bddc9
Piret Valentin <valentin.piret@affinitic.be>
2019-10-03 11:37:58 +0200

    blacked

--- src/renocopro/policy/faceted/professionals.py
+++ src/renocopro/policy/faceted/professionals.py
@@ -5,6 +5,5 @@ from renocopro.policy.utils import translate_selected_taxonomy_item
 
 
 class ProfessionalsView(BrowserView):
-
     def get_taxonomy_item(self, context, taxonomy_id, item_id):
         return translate_selected_taxonomy_item(context, taxonomy_id, item_id)
